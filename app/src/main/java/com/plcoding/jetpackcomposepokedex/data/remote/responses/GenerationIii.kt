830f3101721fd244bd513d85327c9568e1caf68c
Rodrigo Barros <brodrigo87@gmail.com>
2022-09-28 16:14:41 -0300

    pokedexapp

---
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/GenerationIii.kt
@@ -0,0 +1,7 @@
+package com.plcoding.jetpackcomposepokedex.data.remote.responses
+
+data class GenerationIii(
+    val emerald: Emerald,
+    val fireredLeafgreen: FireredLeafgreen,
+    val rubySapphire: RubySapphire
+)
