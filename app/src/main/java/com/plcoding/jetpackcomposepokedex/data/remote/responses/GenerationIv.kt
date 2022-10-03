eee35c8c5c6b116b26aa0fa5fa0f775c50e147a3
Rodrigo Barros <brodrigo87@gmail.com>
2022-10-03 11:44:21 -0300

    generation

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/GenerationIv.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/GenerationIv.kt
@@ -1,7 +1,7 @@
 package com.plcoding.jetpackcomposepokedex.data.remote.responses
 
 data class GenerationIv(
-    val diamond-pearl: DiamondPearl,
-    val heartgold-soulsilver: HeartgoldSoulsilver,
+    val diamondPearl: DiamondPearl,
+    val heartgoldSoulsilver: HeartgoldSoulsilver,
     val platinum: Platinum
 )
