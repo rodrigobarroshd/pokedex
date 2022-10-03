1be38f77af7a7a89ff599d62590336816082434a
Rodrigo Barros <brodrigo87@gmail.com>
2022-10-03 11:49:49 -0300

    generation

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/GenerationVii.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/GenerationVii.kt
@@ -2,5 +2,5 @@ package com.plcoding.jetpackcomposepokedex.data.remote.responses
 
 data class GenerationVii(
     val icons: Icons,
-    val ultra-sun-ultra-moon: UltraSunUltraMoon
+    val ultraSunUltraMoon: UltraSunUltraMoon
 )
