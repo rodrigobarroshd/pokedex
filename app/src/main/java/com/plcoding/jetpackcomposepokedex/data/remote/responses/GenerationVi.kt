b4b4a17a2fa823176c8d86c2ba89bb184278bb13
Rodrigo Barros <brodrigo87@gmail.com>
2022-10-03 11:44:25 -0300

    generation

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/GenerationVi.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/GenerationVi.kt
@@ -1,6 +1,6 @@
 package com.plcoding.jetpackcomposepokedex.data.remote.responses
 
 data class GenerationVi(
-    val omegaruby-alphasapphire: OmegarubyAlphasapphire,
-    val x-y: XY
+    val omegarubyAlphasapphire: OmegarubyAlphasapphire,
+    val xy: XY
 )
