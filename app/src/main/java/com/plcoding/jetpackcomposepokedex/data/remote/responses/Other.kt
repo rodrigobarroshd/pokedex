297ab49a88e55f757e34e36c30bdad796ee6be16
Rodrigo Barros <brodrigo87@gmail.com>
2022-10-03 11:49:58 -0300

    other

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/Other.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/data/remote/responses/Other.kt
@@ -3,5 +3,5 @@ package com.plcoding.jetpackcomposepokedex.data.remote.responses
 data class Other(
     val dream_world: DreamWorld,
     val home: Home,
-    val official-artwork: OfficialArtwork
+    val officialArtwork: OfficialArtwork
 )
